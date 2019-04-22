09e3e5131c223a06f29d440782a0850d0409c295
Karl <42676795+hankarl@users.noreply.github.com>
2019-04-22 10:11:35 -0300

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -1,3 +1,3 @@
 <?php
 
-$conexao = mysqli_connect("localhost", "root", "CarKiv2922", "net1adm") or die('Conexão falhou, erro: '.mysqli_connect_error());
+$conexao = mysqli_connect("localhost", "root", "xxxxxxx", "net1adm") or die('Conexão falhou, erro: '.mysqli_connect_error());
